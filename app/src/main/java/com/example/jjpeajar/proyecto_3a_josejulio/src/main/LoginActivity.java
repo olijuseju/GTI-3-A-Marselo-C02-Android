6c602d52647fba5448a25a7106cbf1f86b5e4c5b
Andrey <akuzmin@epsg.upv.es>
2021-11-11 16:29:49 +0100

    login v.1

---
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.jjpeajar.proyecto_3a_josejulio.src.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.jjpeajar.proyecto_3a_josejulio.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
