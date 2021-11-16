84e5f877937cbbd11ebb350f6277bd6d82340d73
Belén Grande <bgralop@epsg.upv.es>
2021-11-16 10:56:19 +0100

    Commit activity user information v1.2 + activity edit user

---
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/EditUserActivity.java
@@ -0,0 +1,39 @@
+package com.example.jjpeajar.proyecto_3a_josejulio.src.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.example.jjpeajar.proyecto_3a_josejulio.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    private TextInputLayout txtInputLayout;
+    private AutoCompleteTextView dropdowntxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+
+        txtInputLayout = findViewById(R.id.input_town);
+        dropdowntxt = findViewById(R.id.dropDown_txt);
+
+        String[] items = new String[]{
+                "Teulada",
+                "Albaida",
+                "Valencia"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                EditUserActivity.this,
+                R.layout.dropdown_item,
+                items
+        );
+
+        dropdowntxt.setAdapter(adapter);
+    }
+}
