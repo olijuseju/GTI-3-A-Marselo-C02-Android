047bf2888da077892684665a88059c740d3dea94
Andrey <akuzmin@epsg.upv.es>
2021-11-17 01:09:34 +0100

    maquetación + implementación MENU -> DONE

---
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/MenuMainActivity.java
@@ -0,0 +1,87 @@
+package com.example.jjpeajar.proyecto_3a_josejulio.src.main;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.jjpeajar.proyecto_3a_josejulio.R;
+import com.example.jjpeajar.proyecto_3a_josejulio.src.main.graphic.GraphicFragment;
+import com.example.jjpeajar.proyecto_3a_josejulio.src.main.home.HomeFragment;
+import com.example.jjpeajar.proyecto_3a_josejulio.src.main.notification.NotificationFragment;
+import com.example.jjpeajar.proyecto_3a_josejulio.src.main.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuMainActivity extends AppCompatActivity {
+
+    //palette
+    private BottomNavigationView bottomNavigationView;
+    //fragment actual que esta pulsado
+    Fragment currentFragment = null;
+    //transaction del fragment
+    FragmentTransaction ft;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_main);
+
+        //fragment control configuration
+        ft = getSupportFragmentManager().beginTransaction();
+        //cargamos al inicio del activity el fragment principal , HomeFragment
+        currentFragment = new HomeFragment();
+        ft.replace(R.id.content, currentFragment); //remplazar por el que hay (en este caso null)
+        ft.commit(); //just do it
+
+        //findById
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+
+        //actions
+
+        //set menu navigation
+        bottomNavigationView.setBackground(null); //le quito el borde ya que se ve unas lineas de fondo , nuse porque
+        //desactivamos el item del menu numero 2 (od.placeholder)
+        //ya que esta vacio para hacer hueco y que se quede en el medio para el boton de mapa
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+        //detectar pulsado
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) { //si el id del item pulsado (se encuentra en la carpeta menu/bottom_nav_menu.xml)
+                    case R.id.myHome:
+                        currentFragment = new HomeFragment(); //fragment que voy a cargar
+                        ft = getSupportFragmentManager().beginTransaction(); //movimiento del fragment
+                        ft.replace(R.id.content, currentFragment); //remplazar
+                        ft.commit(); //just do it
+                        return true; //salir del switch
+                    case R.id.myGraphics:  // -> lo mismo para lo demás
+                        currentFragment = new GraphicFragment();
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.content, currentFragment);
+                        ft.commit();
+                        return true;
+                    case R.id.myNotifications:
+                        currentFragment = new NotificationFragment();
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.content, currentFragment);
+                        ft.commit();
+                        return true;
+                    case R.id.myProfile:
+                        currentFragment = new ProfileFragment();
+                        ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.content, currentFragment);
+                        ft.commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/graphic/GraphicFragment.java
@@ -0,0 +1,66 @@
+package com.example.jjpeajar.proyecto_3a_josejulio.src.main.graphic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jjpeajar.proyecto_3a_josejulio.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GraphicFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GraphicFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public GraphicFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment GraphicFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static GraphicFragment newInstance(String param1, String param2) {
+        GraphicFragment fragment = new GraphicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_graphic, container, false);
+    }
+}
