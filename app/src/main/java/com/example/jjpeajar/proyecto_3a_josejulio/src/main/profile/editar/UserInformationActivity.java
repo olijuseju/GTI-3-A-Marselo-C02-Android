0b0e41326e6ebfae9a184dfc78af673133e3826b
Belén Grande <bgralop@epsg.upv.es>
2021-11-26 14:20:57 +0100

    Modificacion en botones guía gases

--- app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/editar/UserInformationActivity.java
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/editar/UserInformationActivity.java
@@ -7,16 +7,42 @@ package com.example.jjpeajar.proyecto_3a_josejulio.src.main.profile.editar;
  */
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.jjpeajar.proyecto_3a_josejulio.R;
+import com.example.jjpeajar.proyecto_3a_josejulio.src.main.gasguide.GasInformationActivity;
 
 public class UserInformationActivity extends AppCompatActivity {
+
+    private ConstraintLayout btn_editar;
+    private ConstraintLayout btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
+
+        btn_back = findViewById(R.id.btn_back_userinfo);
+        btn_editar = findViewById(R.id.bt_editar);
+
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btn_editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), EditUserActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
