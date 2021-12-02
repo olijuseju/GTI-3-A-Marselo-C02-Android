a755119a62038072015c459c376e591d373df13c
Jose Julio <jjpeajar@epsg.upv.es>
2021-12-02 14:23:38 +0100

    Implementacion Config

--- app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/config/ConfigurationNotificationsActivity.java
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/config/ConfigurationNotificationsActivity.java
@@ -5,12 +5,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.example.jjpeajar.proyecto_3a_josejulio.R;
 
 public class ConfigurationNotificationsActivity extends AppCompatActivity {
 
     private ConstraintLayout bt_back_config_notif_n;
+    private CheckBox checksis;
+    private CheckBox checknon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,38 @@ public class ConfigurationNotificationsActivity extends AppCompatActivity {
 
         bt_back_config_notif_n = findViewById(R.id.bt_back_config_notif_n);
 
+        checksis=findViewById(R.id.checksis);
+        checknon=findViewById(R.id.checknon);
+
         bt_back_config_notif_n.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        checksis.setChecked(true);
+        checknon.setChecked(false);
+
+        checksis.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checknon.setChecked(false);
+                }else{
+                    checknon.setChecked(true);
+                }
+            }
+        });
+
+        checknon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checksis.setChecked(false);
+                }else{
+                    checksis.setChecked(true);
+                }
             }
         });
     }
--- app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/config/ConfigurationThemeActivity.java
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/profile/config/ConfigurationThemeActivity.java
@@ -5,6 +5,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.example.jjpeajar.proyecto_3a_josejulio.R;
 
@@ -12,6 +14,9 @@ public class ConfigurationThemeActivity extends AppCompatActivity {
 
 
     private ConstraintLayout bt_back_config;
+    private CheckBox checksis;
+    private CheckBox checknon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,38 @@ public class ConfigurationThemeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuration_theme);
 
         bt_back_config = findViewById(R.id.bt_back_config_t);
+        checksis=findViewById(R.id.checkman);
+        checknon=findViewById(R.id.checkd2);
 
         bt_back_config.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        checksis.setChecked(true);
+        checknon.setChecked(false);
+
+        checksis.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checknon.setChecked(false);
+                }else{
+                    checknon.setChecked(true);
+                }
+            }
+        });
+
+        checknon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checksis.setChecked(false);
+                }else{
+                    checksis.setChecked(true);
+                }
             }
         });
 
