55dada6874cfff0a134c1d8a397628bacae538a7
Belén Grande <bgralop@epsg.upv.es>
2022-01-08 12:42:23 +0100

    MAPA DENGUE PUTO FUNCIONA

--- app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/map/MapActivity.java
+++ app/src/main/java/com/example/jjpeajar/proyecto_3a_josejulio/src/main/map/MapActivity.java
@@ -3,6 +3,8 @@ package com.example.jjpeajar.proyecto_3a_josejulio.src.main.map;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -17,9 +19,16 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+
         myWebView = findViewById(R.id.webview);
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://vmi621282.contaboserver.net/mapa");
+        myWebView.setWebChromeClient(new WebChromeClient());
+
+        myWebView.loadUrl("http://vmi621282.contaboserver.net/mapaMovil");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
 
     }
 }
